0bc4607f3ffbd5d4c78dd0474825c4f42ad078d9
coder212 <yujimaarif.ym@gmail.com>
2014-03-10 10:31:57 +0900

    menu_action_refresh fungsi

--- Bangkong/src/com/kodok/bangkong/MainActivity.java
+++ Bangkong/src/com/kodok/bangkong/MainActivity.java
@@ -3,6 +3,7 @@ package com.kodok.bangkong;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -27,6 +28,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		if(item.getItemId()==R.id.action_settings){
+			view.reload();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
