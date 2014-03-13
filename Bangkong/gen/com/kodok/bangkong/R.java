59181fb3c351777cfe90ca5f21e076afa5a823be
coder212 <yujimaarif.ym@gmail.com>
2014-03-13 16:59:45 +0900

    progr

--- Bangkong/gen/com/kodok/bangkong/R.java
+++ Bangkong/gen/com/kodok/bangkong/R.java
@@ -25,11 +25,13 @@ public final class R {
         public static final int ic_menu_refresh=0x7f020001;
     }
     public static final class id {
-        public static final int action_refresh=0x7f040001;
+        public static final int action_refresh=0x7f040002;
+        public static final int progressBar1=0x7f040001;
         public static final int wview=0x7f040000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int proglay=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- Bangkong/src/com/kodok/bangkong/MainActivity.java
+++ Bangkong/src/com/kodok/bangkong/MainActivity.java
@@ -1,24 +1,31 @@
 package com.kodok.bangkong;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	WebView view;
 	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final Activity context;
+		context = this;
 		ConnectivityManager cn = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo ni = cn.getActiveNetworkInfo();
 		setTitle("Cipta Group");
@@ -27,7 +34,64 @@ public class MainActivity extends Activity {
 		setting.setJavaScriptEnabled(true);
 		if(ni != null && ni.isConnected()==true){
 			view.loadUrl("http://www.cargobatam.com/index.php");
-			view.setWebViewClient(new WebviewClients());
+			view.setWebViewClient(new WebViewClient(){
+				ProgressDialog pd = new ProgressDialog(context);
+
+				/* (non-Javadoc)
+				 * @see android.webkit.WebViewClient#onPageStarted(android.webkit.WebView, java.lang.String, android.graphics.Bitmap)
+				 */
+				@Override
+				public void onPageStarted(WebView view, String url,
+						Bitmap favicon) {
+					// TODO Auto-generated method stub
+					super.onPageStarted(view, url, favicon);
+					pd.setMessage("lading the file... be patient");
+					pd.setCancelable(false);
+					pd.show();
+					
+				}
+
+				/* (non-Javadoc)
+				 * @see android.webkit.WebViewClient#onPageFinished(android.webkit.WebView, java.lang.String)
+				 */
+				@Override
+				public void onPageFinished(WebView view, String url) {
+					// TODO Auto-generated method stub
+					super.onPageFinished(view, url);
+					if(pd.isShowing())
+					     pd.dismiss();
+				}
+
+				/* (non-Javadoc)
+				 * @see android.webkit.WebViewClient#shouldOverrideUrlLoading(android.webkit.WebView, java.lang.String)
+				 */
+				@Override
+				public boolean shouldOverrideUrlLoading(WebView view, String url) {
+					// TODO Auto-generated method stub
+					pd.setMessage("lading the file... be patient");
+					pd.setCancelable(false);
+					pd.show();
+					view.loadUrl("http://www.cargobatam.com/index.php");
+					return true;
+				}
+				
+				
+			});
+			view.setWebChromeClient(new WebChromeClient(){
+
+				/* (non-Javadoc)
+				 * @see android.webkit.WebChromeClient#onProgressChanged(android.webkit.WebView, int)
+				 */
+				@Override
+				public void onProgressChanged(WebView view, int newProgress) {
+					//  Auto-generated method stub
+					super.onProgressChanged(view, newProgress);
+					context.setProgress(newProgress*1000);
+					 
+					 
+				}
+				
+			});
 		}else{
 			TextView txt = new TextView(this);
 			txt.setText("Tidak ada koneksi internet");
